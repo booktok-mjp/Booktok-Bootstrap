568fcda0f352dce0090fe0fd03c75f1ba845f5cf
marlisapee <mpee@tripadvisor.com>
2024-11-05 12:59:38 -0500

    color constant configs

---
+++ src/config/constants.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'Book Tok',
+  tagline: 'Chat about your latest read!',
+  tagline2: 'Discover, share, connect.',
+  signupLine: 'Join a community of book lovers!',
+  myBookcase: 'My Bookcase',
+  myMinicase: 'My Minicase',
+  recommendations: 'Recommendations for You',
+};
