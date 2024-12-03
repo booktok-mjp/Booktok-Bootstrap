29fdb9299310bbad50609b820036b53eda606c37
marlisapee <mpee@tripadvisor.com>
2024-12-03 16:13:02 -0500

    css changes, logic for handleAdd

--- src/components/card/CustomCard.jsx
+++ src/components/card/CustomCard.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
 import { BiBookAdd } from 'react-icons/bi';
 import { FaBookReader } from 'react-icons/fa';
 
-import './CustomCard.css';
 import ReactIconButton from '../button/ReactIconButton';
 import { Colors } from '../../config';
+import { addBookToBookcase } from '../../services/bookcaseService';
+import BodyText from '../typography/BodyText';
 
-const CustomCard = ({ book, showAddBtn = true }) => {
+import './CustomCard.css';
+
+const CustomCard = ({
+  book,
+  showAddBtn = true,
+  setAlert,
+  setShowAlert,
+  fetchBookcase,
+}) => {
+  const { getAccessTokenSilently } = useAuth0();
   const navigate = useNavigate();
 
   const handleNavigate = () => {
     navigate(`/book/${book.id}`);
   };
 
-  const handleAddToBookcase = () => {
-    console.log('add book to bookcase');
+  const handleAddToBookcase = async (bookId) => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await addBookToBookcase({ token, bookId });
+      if (response.status === 400) {
+        setAlert({
+          heading: 'Oh no!',
+          body: 'An error occured while adding this book to your bookcase. Please try again!',
+          variant: 'danger',
+        });
+        setShowAlert(true);
+      } else {
+        await fetchBookcase();
+      }
+    } catch (error) {
+      console.error('Error adding book', error);
+    }
   };
 
   const handleSetReadingNow = (bookId) => {
@@ -40,9 +65,10 @@ const CustomCard = ({ book, showAddBtn = true }) => {
         style={{ backgroundColor: Colors.ivory }}
       >
         <Card.Title>{book.title}</Card.Title>
-        <Card.Text className="flex-grow-1">
-          {book.description.substring(0, 80)}...
-        </Card.Text>
+        <BodyText
+          text={`${book.description.substring(0, 70)}...`}
+          className="flex-grow-1"
+        />
       </Card.Body>
       <ListGroup className="list-group-flush bg-body-tertiary">
         <ListGroup.Item>{book.author_name}</ListGroup.Item>
@@ -54,7 +80,7 @@ const CustomCard = ({ book, showAddBtn = true }) => {
         {showAddBtn ? (
           <ReactIconButton
             icon={<BiBookAdd />}
-            onClick={handleAddToBookcase}
+            onClick={() => handleAddToBookcase(book.id)}
             tooltipText="Add to Bookcase"
           />
         ) : (
