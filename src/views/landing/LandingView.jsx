3f6871b398d07246c93119327c12c5e26c371741
marlisapee <mpee@tripadvisor.com>
2024-12-09 15:34:39 -0500

    landing view css changes

--- src/views/landing/LandingView.jsx
+++ src/views/landing/LandingView.jsx
@@ -1,13 +1,18 @@
 import LandingEnterButton from '../../components/button/LandingEnterButton';
+import CustomHeader from '../../components/header/CustomHeader';
 import LandingHeader from '../../components/header/LandingHeader';
+import { Constants } from '../../config';
 
 import './LandingView.css';
 
 const LandingView = () => {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center h-100 w-100 landing-page">
+    <div className="landing-page">
       <LandingHeader />
       <LandingEnterButton />
+      <div className="mt-4">
+        <CustomHeader text={Constants.tagline2} />
+      </div>
     </div>
   );
 };
