7134182a8e133d6f5e03376a0988ed3b74671c66
marlisapee <mpee@tripadvisor.com>
2024-12-09 15:38:29 -0500

    add create thread button

--- src/views/discussion/DiscussionView.jsx
+++ src/views/discussion/DiscussionView.jsx
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { BiSolidMessageAdd, BiSolidMessageX } from 'react-icons/bi';
 
 import CustomHeader from '../../components/header/CustomHeader';
 import useThreads from '../../hooks/useThreads';
 import ThreadList from '../../components/thread/ThreadList';
-
+import LoadingSpinner from '../../components/spinner/LoadingSpinner';
+import ReactIconButton from '../../components/button/ReactIconButton';
+import ThreadForm from '../../components/form/ThreadForm';
+import Divider from '../../components/divider/Divider';
 import { Colors, Constants } from '../../config';
+
+import './DiscussionView.css';
 
 const DiscussionView = () => {
   const { allThreads, myThreads, error, fetchThreads, loading } = useThreads();
+  const [showForm, setShowForm] = useState(false);
 
-  console.log('threads in view', allThreads);
-  console.log('my threads:', myThreads);
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   return (
-    <Container className="justify-content-center">
-      <Row>
-        <CustomHeader text={Constants.discussions} size="xlg" />
+    <Container>
+      <Row className="mb-3">
+        <CustomHeader text="Chat with Booktok!" size="xlg" />
       </Row>
       <Row className="justify-content-around">
         {/* my threads */}
-        <Col lg={6}>
-          <div className="mb-3">
-            <CustomHeader color={Colors.wineRed} text="My Threads" size="md" />
+        <Col className="thread-container-discussion h-50" lg={5}>
+          <div className="d-flex justify-content-between mb-3">
+            <CustomHeader
+              color={Colors.darkSlate}
+              text="My Threads"
+              size="md"
+            />
+            {showForm ? (
+              <ReactIconButton
+                onClick={() => setShowForm(false)}
+                icon={<BiSolidMessageX />}
+                tooltipText="Cancel"
+              />
+            ) : (
+              <ReactIconButton
+                icon={<BiSolidMessageAdd />}
+                onClick={() => setShowForm(!showForm)}
+                tooltipText="Add new thread"
+              />
+            )}
           </div>
+          {showForm && (
+            <ThreadForm fetchThreads={fetchThreads} setShowForm={setShowForm} />
+          )}
           <ThreadList threads={myThreads} />
         </Col>
+
         {/* all threads */}
-        <Col lg={6}>
+        <Col className="thread-container-discussion h-50" lg={6}>
           <div className="mb-3">
             <CustomHeader color={Colors.wineRed} text="Discover" size="md" />{' '}
           </div>
