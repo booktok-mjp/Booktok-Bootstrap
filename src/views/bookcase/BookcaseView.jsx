8463bf565219db475d3c13ce53a34b13d9a7ecab
marlisapee <mpee@tripadvisor.com>
2024-12-09 15:40:55 -0500

    bookcase updates

--- src/views/bookcase/BookcaseView.jsx
+++ src/views/bookcase/BookcaseView.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import CustomHeader from '../../components/header/CustomHeader';
 import CustomCard from '../../components/card/CustomCard';
 import useBookcase from '../../hooks/useBookcase';
+import BookDetailCard from '../../components/card/BookDetailCard';
 
 import './BookcaseView.css';
-import BookDetailCard from '../../components/card/BookDetailCard';
-import CustomGrid from '../../components/grid/CustomGrid';
+import LoadingSpinner from '../../components/spinner/LoadingSpinner';
+import { Colors } from '../../config';
 
 const BookcaseView = () => {
-  const { books } = useBookcase();
+  const { books, loading, fetchBookcase } = useBookcase();
 
   // add functionality
-  const readingNow = 4;
+  const readingNow = 2;
   const readingNowBook = books.find((book) => book.id === readingNow);
 
   const customBookCards =
     books &&
     books.map((book) => (
-      <CustomCard key={book.id} book={book} showAddBtn={false} />
+      <CustomCard
+        book={book}
+        fetchBookcase={fetchBookcase}
+        isBookcaseView={true}
+        key={book.id}
+        showAddBtn={false}
+      />
     ));
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <Container className="bookcase-view-container">
-      <CustomHeader text="My Bookcase" size="xlg" />
-      <Row>
-        <Col lg={4} xs={12}>
-          {readingNowBook && <BookDetailCard currentBook={readingNowBook} />}
+      <Row className="mb-4">
+        <CustomHeader color={Colors.navyBlue} text="My Bookcase" size="xlg" />
+      </Row>
+      <Row className="reading-now-container">
+        <Col className="reading-now sticky-col" lg={5} xs={12}>
+          <CustomHeader text="Reading Now..." />
+          {readingNowBook && (
+            <BookDetailCard
+              isBookcaseView={true}
+              currentBook={readingNowBook}
+            />
+          )}
         </Col>
-        <Col lg={8} xs={12} className="d-flex flex-wrap justify-content-evenly">
+        <Col lg={7} xs={12} className="d-flex flex-wrap justify-content-around">
           {customBookCards}
         </Col>
       </Row>
