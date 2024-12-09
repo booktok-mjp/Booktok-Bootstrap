164785276bf27af539a79823a16a1756af223fc6
marlisapee <mpee@tripadvisor.com>
2024-12-09 15:38:44 -0500

    use constant

--- src/components/header/LandingHeader.jsx
+++ src/components/header/LandingHeader.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './LandingHeader.css';
+import { Constants } from '../../config';
 
 const LandingHeader = () => {
-  return <div className="pacifico-regular-header">Welcome to Booktok</div>;
+  return <div className="pacifico-regular-header">{Constants.welcome}</div>;
 };
 
 export default LandingHeader;
